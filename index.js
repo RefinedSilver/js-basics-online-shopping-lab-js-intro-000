8de18d0975284aaf0b8f3fc71345ed5f55667c6f
Ian McKenzie <imckenzie777@gmail.com>
2020-03-24 05:36:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -77,11 +77,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   let cartTotal = total();
-
-	if(!cardNumber) {
-  	return `Sorry, we don't have a credit card on file for you.`;
-  } else {
-  	console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+	if(cardNumber !== undefined) {
     cart = [];
+  	return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
+  } else {
+  	return `Sorry, we don't have a credit card on file for you.`;
   }
 }
