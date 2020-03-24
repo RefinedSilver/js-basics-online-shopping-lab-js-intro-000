f8a69bbbc00edca0cdf1ecf43dd8c8b5c381814e
Ian McKenzie <imckenzie777@gmail.com>
2020-03-24 05:37:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function placeOrder(cardNumber) {
   // let cartTotal = total();
 	if(cardNumber !== undefined) {
     cart = [];
-  	return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  	return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
   } else {
   	return `Sorry, we don't have a credit card on file for you.`;
   }
